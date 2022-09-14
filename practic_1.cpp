613c3b78ad4153d638506704529336a9dd74e142
Danilo <82391486+Danylo-Honcharenko@users.noreply.github.com>
2022-09-14 14:42:51 +0300

    Add files via upload

---
+++ practic_1.cpp
@@ -0,0 +1,17 @@
+//
+// Created by danil on 14.09.22.
+//
+
+#include "iostream"//;
+#include "cmath"//;
+
+using namespace std;
+
+int main() {
+    float a, Z1;
+    cout << "Введите величину угла альфа в радианах: ";
+    cin >> a;
+    Z1 = sin(2 * a) + sin(5 * a) - sin(3 * a)/cos(a) - cos(3 * a) + cos(5 * a);
+    cout << "Значение Z1 равно " << Z1;
+    return 0;
+}
