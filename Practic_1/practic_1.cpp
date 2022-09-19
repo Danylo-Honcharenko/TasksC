cf8629654791af62cf902d1170e16b2153055985
Danilo <82391486+Danylo-Honcharenko@users.noreply.github.com>
2022-09-19 17:42:37 +0300

    Практическая номер 1

--- Practic_1/practic_1.cpp
+++ Practic_1/practic_1.cpp
@@ -11,7 +11,7 @@ int main() {
     float a, Z1, Z2;
     cout << "Введите величину угла альфа в радианах: ";
     cin >> a;
-    Z1 = sin(2 * a) + sin(5 * a) - sin(3 * a)/cos(a) - cos(3 * a) + cos(5 * a);
+    Z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a))/(cos(a) - cos(3 * a) + cos(5 * a));
     Z2 = tan(3 * a);
     cout << "Значение Z1 равно " << Z1 << endl;
     cout << "Значение Z2 равно " << Z2;
